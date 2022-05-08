a615766275cb93bb5929f8d52965435fe833ed99
EthanChen0802 <u83kw4@gmail.com>
2022-05-08 18:37:42 +0800

    Revert "yahoo股市爬蟲, 加入pandas模組練習"
    This reverts commit de5f030e

--- yahoo_stock.py
+++ yahoo_stock.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,18 +7,8 @@ if r.status_code == requests.codes.ok:
     soup = BeautifulSoup(r.text, 'html.parser')
     result_set = soup.find_all("ul")[16].select('li')
 
-    title_list = []
-    content_list = []
-
     for i in result_set:
         column = i.find_all('span')
         title = column[0].text
         content = column[1].text
-        title_list.append(title)
-        content_list.append(content)
-
-    stock_name = soup.find('h1').text
-    pd.set_option('display.max_columns', 20)
-    pd.set_option('display.width', 1000)
-    df = pd.DataFrame(content_list, index=title_list, columns=[stock_name])
-    print(df)
+        print(f'{title}: {content}')
